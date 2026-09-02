1e4b387266f4d41b83e61976c890b2c514a9db4e
Felipe <felipesaziki@gmail.com>
2026-09-02 16:43:13 -0300

    Adicionado src e demais necessidades

---
+++ Back-end/src/database/conexao.py
@@ -0,0 +1,12 @@
+from mysql import connector
+from settings import settings
+
+
+def conectar():
+    return connector.connect(
+        host=settings.db_host,
+        port=settings.db_port,
+        user=settings.db_user,
+        password=settings.db_password,
+        database=settings.db_name
+    )
